6875e098a2d7b1fa0deff0232d3e5dd06307e3a7
Luís Pinto <Luspin@microsoft.com>
2024-01-23 14:48:20 +0000

    new test

--- taskpane.js
+++ taskpane.js
@@ -1,15 +1,16 @@
-let emailSaveStatusBanner = document.getElementById("emailSaveStatusBanner")
 
 Office.onReady(async (info) => {
     console.log("Office.onReady called");
 
+    let emailSaveStatusBanner = document.getElementById("emailSaveStatusBanner")
+
     if (info.host === Office.HostType.Outlook) {
-        await checkEmailSaveStatus(Office.context.mailbox.item.itemId);
+        await checkEmailSaveStatus(emailSaveStatusBanner);
     }
 });
 
 
-async function checkEmailSaveStatus() {
+async function checkEmailSaveStatus(emailSaveStatusBanner) {
     console.log("Checking email save status");
 
     emailSaveStatusBanner.textContent  = "Checking email save status...";
